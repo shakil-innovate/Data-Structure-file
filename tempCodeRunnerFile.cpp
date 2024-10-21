3bba1662f2ac568880b5d0ba5098a800a233f2a8
shakil-innovate <shakil.innovate@gmail.com.>
2024-10-21 12:14:13 +0600

    modified quick Sort

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int partition(vector<int>&v,int low,int high)
+int partition(vector<int>v,int low,int high)
 {
    int pi=v[high];
    int i=low-1;
@@ -26,23 +26,6 @@ void quick_Sort(vector<int>&v,int low,int high)
   if(low<high)
   {
     pi=partition(v,low,high);
-    quick_Sort(v,low,pi-1);
-    quick_Sort(v,pi+1,high);
-    
   }
 }
 
-
-int main()
-{
-    vector<int>arr={75,30,41,12,85,25,60,55,20,11,0};
-
-    int siz=arr.size();
-
-    quick_Sort(arr,0,siz-1);
-
-    for(int i=0;i<siz;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-}
