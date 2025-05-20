5df28fe4b9d5d501e7fa6c024ba364fa69a2c919
shakil-innovate <shakil.innovate@gmail.com>
2025-05-20 19:19:57 +0600

    Update kmp.cpp

--- string_processing/kmp.cpp
+++ string_processing/kmp.cpp
@@ -1,48 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-string s;  
+
 vector<int>pie;
-bool flag;
-string pattern;
+string pattern,s;
+bool flag=false;
 
-void mass()
+void mos()
 {
+
     int j=0;
+     for(int i=0;i<s.size();)
+     {
+        if(s[i]==pattern[j+1])
+        {
+            i++,j++;
+        }
+        else
+        {
+           if(j!=0) j=pie[j];
+           else i++;
+        }
 
-  for(int i=0;i<s.length();)
-  {
-    if(s[i]==pattern[j+1])
-    {
-      j++;i++;
-    }
-    else if(j!=0)   j=pie[j];
-    else   if(j==0)  {j=0;i++;}
-    if(j==pattern.length()-1){flag=true;return;}
-  }
+        if(j==pattern.size()-1)
+        {
+            cout<<"index no : "<<i-j<<endl;
+            flag=true;return;
+        }
 
-  if(j==pattern.length()-1){flag=true;}
+     }
 }
 
 int main()
 {
-      cin>>s;
-      cin>>pattern;    pattern=" "+pattern;   
+    cin>>s>>pattern;
 
-  int n=pattern.length();
-    pie.resize(n+1,0);
-     
-     int j=0;
+    pattern=" "+pattern;
+    pie.resize(pattern.length()+1,0);
 
-    for(int i=1;i<=n;i++)
+    int j=1;
+
+    for(int i=2;i<pattern.size();i++)
     {
-       if(pattern[i]==pattern[j]){
-        pie[i]=j++;
-       }
-       else j=0;
+        if(pattern[i]==pattern[j])
+        {
+            pie[i]=j;
+            j++;
+        }
+        else
+        {
+             j=1;
+        }
     }
 
-    flag=false;
-   mass();
+    mos();
 
-   cout<<(flag?"YES":"NO")<<endl;
+    cout<<(flag?"YES":"NO")<<endl;
+
+
 }
