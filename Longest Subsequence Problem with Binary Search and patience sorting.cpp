9820831bd40c60810a1855106ef9d1a8fd1d5de9
shakil-innovate <shakil.innovate@gmail.com>
2025-07-16 16:33:49 +0600

    Update Longest Subsequence Problem with save sequence

--- Longest Subsequence Problem with Binary Search and patience sorting.cpp
+++ Longest Subsequence Problem with Binary Search and patience sorting.cpp
@@ -1,23 +1,50 @@
     #include <bits/stdc++.h>
     using namespace std;
     typedef long long ll;
-
-    ll lis(vector<ll>&v)
+    vector<ll>subs,real_ans;
+    vector<pair<ll,ll>>rem;
+    int cnt=0;
+    
+    void lis(vector<ll>&v)
     {
-        vector<ll>sub;
-
+        vector<ll>real_anss;
+        
         for(auto num:v)
         {
-            auto pos=upper_bound(v.begin(),v.end(),num);
-            if(pos==v.end())sub.push_back(num);
-            else *pos=num;
+           auto it=lower_bound(subs.begin(),subs.end(),num);
+           if(it==subs.end())
+           {
+            subs.push_back(num);
+            cnt=0;
+           }
+           else 
+            {
+                int i=it-subs.begin();
+                rem.push_back({num,i});
+                cnt++;
+                *it=num;
+            }
         }
-        return sub.size();
+        real_ans=real_anss;
     }
+
+
     void solve()
     {
-        vector<ll>v={10,20,1,30,2,3,4};
-        cout<<lis(v)<<endl;
+        vector<ll>v{10,20,1,30,2,3,4};
+        lis(v);
+        cout<<subs.size()<<endl;
+
+        while(cnt>0)
+        {
+            auto [x,y]=rem.back();
+            subs[y]=x;
+            cnt--;
+            rem.pop_back();
+        }
+        for(auto it:subs)
+        cout<<it<<" ";
+        cout<<endl;
     }
 
     int main()
