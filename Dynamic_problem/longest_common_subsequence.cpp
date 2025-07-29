071f214f9759de1d8c405e0f47b4c64b7e1d4ead
shakil-innovate <shakil.innovate@gmail.com>
2025-07-29 22:36:31 +0600

    Create longest_common_subsequence by tabular method

---
+++ Dynamic_problem/longest_common_subsequence.cpp
@@ -0,0 +1,44 @@
+problem link:https://atcoder.jp/contests/dp/tasks/dp_f
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+    string s,t;     cin>>s>>t;
+    vector<vector<ll>>dp(s.size()+1,vector<ll>(t.size()+1,0));
+
+    for(int i=1 ; i<=s.size() ; i++)
+    {
+        for(int j=1; j<=t.size() ; j++)
+        {
+            if(s[i-1]==t[j-1])dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+
+    string lcm="";
+
+    int i=s.size(),j=t.size();
+
+    while(i>0 && j>0)
+    {
+        if(s[i-1]==t[j-1]){lcm+=s[i-1];i--,j--;}
+        else if(dp[i-1][j] > dp[i][j-1])i--;
+        else j--;
+    }
+
+    reverse(lcm.begin(),lcm.end());
+
+    cout<<lcm<<endl;
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+    solve();
+}
