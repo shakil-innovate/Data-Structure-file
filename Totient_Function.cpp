614adfdea42a052e69d86dce08cda29e77f00a52
shakil-innovate <shakil.innovate@gmail.com>
2025-08-21 13:20:16 +0600

    Update Totient_Function.cpp

--- Totient_Function.cpp
+++ Totient_Function.cpp
@@ -1,47 +1,74 @@
-#include <bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef double db;
+#define nl '\n'
 
-ll ph(ll n)
+void linearSieve(ll n,vector<ll>&spf,vector<ll>&primes)
 {
-    ll result=n;
+  spf.resize(n+1,0);
+  primes.clear();
 
-    ll p;
-    for(p=2; p*p<=n ; p++)
+  for(int i=2;i<=n;i++)
+  {
+    if(spf[i]==0)
     {
-        if(n%p==0)
-        {
-            while(n%p==0)
-            {
-                n/=p;
-                result-=result/p;
-            }
-        }
+      primes.push_back(i);
+      spf[i]=i;
     }
-
-    // cout<<result<<" "<<n<<" "<<p<<endl;
-
-    if(n>1)
+    for(auto &p:primes)
     {
-        result-=result/n;
+      if(p*i*1LL>n || p>spf[i] )break;
+      spf[i*p]=p;
     }
-
-    return result;
+  }
 }
 
-void solve()
+vector<ll> factorize(ll x,vector<ll>&spf)
 {
-    ll n;   cin>>n;
-
-    cout<<ph(n)<<endl;
+    vector<ll>factors;
+    while(x>1)
+    {
+      factors.push_back(spf[x]);
+      x/=spf[x];
+    }
+    return factors;
 }
 
-int main() {
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
+void shakil()
+{
+    ll n; cin>>n;
 
-//    ll tt;   cin>>tt;
-   
-//    while(tt--)
-     solve();
+    vector<ll>primes,spf;
+    linearSieve(n,spf,primes);
+
+    ll phi=n;
+    vector<ll>factor=factorize(n,spf);
+
+
+    set<ll>distinct;
+    for(auto it:factor)
+    {
+      distinct.insert(it);
+    }
+
+    for(auto p:distinct)
+    {
+      phi-=phi/p;
+    }
+    cout<<phi<<nl;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  ll tt=1;
+  cin>>tt;
+
+  for(int i=1;i<=tt;i++)
+  {
+    shakil();
+  }
 }
