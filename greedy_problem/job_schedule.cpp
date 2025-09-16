76fc875dab988789966ec211c9fbebab446d6192
shakil-innovate <shakil.innovate@gmail.com>
2025-09-16 12:02:39 +0600

    Update job_schedule.cpp

--- greedy_problem/job_schedule.cpp
+++ greedy_problem/job_schedule.cpp
@@ -8,12 +8,6 @@ const ll mod=1e9+7;
 const ll INF=1e9;
 #define vi vector<ll>
 #define vii vector<vi>
-
-ll add(ll a,ll b){ return ((a%mod + b%mod)+ mod ) % mod;}
-ll sub(ll a,ll b){ return ((a%mod - b%mod)+mod)% mod;}
-ll mul(ll a,ll b){ return ((a%mod) * (b%mod))%mod ;}
-ll power(ll a, ll b){ll res=1; a%=mod;while(b > 0){if(b & 1)res=(res*a) %mod; a=(a*a)%mod;b=b>>1;}return res;}
-ll inv(ll a){return power(a,mod-2);}
 //---------------------------------------//
 
 struct State
