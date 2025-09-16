7ad2212efa459f9fe43f89241606174b63a93905
shakil-innovate <shakil.innovate@gmail.com>
2025-09-16 16:06:32 +0600

    Update Convex_hull_Graham_scan.cpp

--- Geometry/Convex_hull_Graham_scan.cpp
+++ Geometry/Convex_hull_Graham_scan.cpp
@@ -11,12 +11,6 @@ const ll INF=1e9;
 #define vii vector<vi>
 #define f first
 #define s second
-
-ll add(ll a,ll b){ return ((a%mod + b%mod)+ mod ) % mod;}
-ll sub(ll a,ll b){ return ((a%mod - b%mod)+mod)% mod;}
-ll mul(ll a,ll b){ return ((a%mod) * (b%mod))%mod ;}
-ll power(ll a, ll b){ll res=1; a%=mod;while(b > 0){if(b & 1)res=(res*a) %mod; a=(a*a)%mod;b=b>>1;}return res;}
-ll inv(ll a){return power(a,mod-2);}
 //---------------------------------------//
 
 struct Point
