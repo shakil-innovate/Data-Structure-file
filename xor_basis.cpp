2993cfef7efab82017c9f9e5268facd7d3b5894f
shakil-innovate <shakil.innovate@gmail.com>
2026-01-04 09:57:29 +0600

    Add XOR basis algorithm in xor_basis.cpp
    Implement XOR basis algorithm to find maximum XOR subset.

---
+++ xor_basis.cpp
@@ -0,0 +1,54 @@
+//problem:https://cses.fi/problemset/task/3191/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define longd long double
+#define vl vector<ll>
+ll INF=1e10;
+
+void shakil(){	
+	ll n;	cin>>n;
+	vl v(n);
+
+	for(auto &i:v)	cin>>i;
+	vl basis(63,0);
+
+	for(auto x:v){
+		for(ll i=62;i>=0;i--){
+			if(((x>>i) & 1)==0) continue;
+
+			if(basis[i]==0){
+				basis[i]=x;
+				break;
+			}
+
+			x^=basis[i];
+		}
+	}
+
+   ll ans=0;
+
+   for(ll i=62;i>=0;i--){
+   	ans=max(ans,ans^basis[i]);
+   }
+
+   cout<<ans<<nl;
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0),cin.tie(0);
+
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+
+	ll tt=1;
+	// cin>>tt;
+
+	while(tt--){
+		shakil();
+	}
+}
