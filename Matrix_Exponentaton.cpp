ac42955fdd838cf93ffda229206bfc7a23780f52
shakil-innovate <shakil.innovate@gmail.com>
2026-01-22 08:04:36 +0600

    Refactor matrix multiplication and power functions

--- Matrix_Exponentaton.cpp
+++ Matrix_Exponentaton.cpp
@@ -11,40 +11,38 @@ struct Matrix{
 };
 
 Matrix multiply(Matrix x,Matrix y){
-    Matrix r;
-    r.a[0][0]=(x.a[0][0] * y.a[0][0] + x.a[0][1]*y.a[1][0])%mod;
-    r.a[0][1]=(x.a[0][0] * y.a[0][1] + x.a[0][1]*y.a[1][1])%mod;
-    r.a[1][0]=(x.a[1][0] * y.a[0][0] + x.a[1][1]*y.a[1][0])%mod;
-    r.a[1][1]=(x.a[1][0] * y.a[0][1] + x.a[1][1]*y.a[1][1])%mod;
-
-    return r;
-
+ Matrix r;
+ r.a[0][0]=(x.a[0][0] * y.a[0][0] + x.a[0][1]*y.a[1][0])%mod;
+ r.a[0][1]=(x.a[0][0] * y.a[0][1] + x.a[0][1]*y.a[1][1])%mod;
+ r.a[1][0]=(x.a[1][0] * y.a[0][0] + x.a[1][1]*y.a[1][0])%mod;
+ r.a[1][1]=(x.a[1][0] * y.a[0][1] + x.a[1][1]*y.a[1][1])%mod;
+    
+ return r;
 }
 
-Matrix power(Matrix base,long long exp){
-    Matrix rs={{{1,0},{0,1}}};
-    while(exp){
-        if(exp & 1) rs=multiply(rs,base);
-        base=multiply(base,base);
-        exp=exp>>1;
-    }
-    return rs;
+Matrix power(Matrix base,ll exp){
+  Matrix rs={{{1,0},{0,1}}};
+  while(exp){
+   if(exp & 1) rs=multiply(rs,base);
+    base=multiply(base,base);
+    exp=exp>>1;
+   }
+  return rs;
 }
 
 ll fib(ll n){
+ if(n==0)return 0;
+ else if(n==1)   return 1;
 
-    if(n==0)return 0;
-    else if(n==1)   return 1;
+ Matrix M=  {{{0,1},{1,1}}};
+ Matrix Mn=power(M,n-1);
 
-    Matrix M=  {{{0,1},{1,1}}};
-    Matrix Mn=power(M,n-1);
-
-    return Mn.a[1][1];
+ return Mn.a[1][1];
 }
 
 void shakil(){
-    ll n;       cin>>n;
-    cout<<fib(n)<<nl;
+ ll n;       cin>>n;
+ cout<<fib(n)<<nl;
 }
 
 int main() {
