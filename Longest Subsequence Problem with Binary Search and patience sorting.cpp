7fc0c497356b4bec9f0394b4f73470ca39478cc6
shakil-innovate <shakil.innovate@gmail.com>
2026-01-22 08:19:38 +0600

    Update Longest Subsequence Problem with Binary Search and patience sorting.cpp

--- Longest Subsequence Problem with Binary Search and patience sorting.cpp
+++ Longest Subsequence Problem with Binary Search and patience sorting.cpp
@@ -5,50 +5,43 @@
     vector<pair<ll,ll>>rem;
     int cnt=0;
     
-    void lis(vector<ll>&v)
-    {
-        vector<ll>real_anss;
+void lis(vector<ll>&v){
+ vector<ll>real_anss;
         
-        for(auto num:v)
-        {
-           auto it=lower_bound(subs.begin(),subs.end(),num);
-           if(it==subs.end())
-           {
-            subs.push_back(num);
-            cnt=0;
-           }
-           else 
-            {
-                int i=it-subs.begin();
-                rem.push_back({num,i});
-                cnt++;
-                *it=num;
-            }
-        }
-        real_ans=real_anss;
-    }
+ for(auto num:v){
+  auto it=lower_bound(subs.begin(),subs.end(),num);
+  if(it==subs.end()){
+   subs.push_back(num);
+    cnt=0;
+  }
+  else{
+  int i=it-subs.begin();
+  rem.push_back({num,i});
+  cnt++;
+  *it=num;
+  }
+ }
+ real_ans=real_anss;
+}
 
+void solve(){
+ vector<ll>v{10,20,1,30,2,3,4};
+ lis(v);
+ cout<<subs.size()<<endl;
 
-    void solve()
-    {
-        vector<ll>v{10,20,1,30,2,3,4};
-        lis(v);
-        cout<<subs.size()<<endl;
+  while(cnt>0){
+  auto [x,y]=rem.back();
+  subs[y]=x;
+  cnt--;
+  rem.pop_back();
+  }
+  
+  for(auto it:subs)
+    cout<<it<<" ";
+    cout<<endl;
+  }
 
-        while(cnt>0)
-        {
-            auto [x,y]=rem.back();
-            subs[y]=x;
-            cnt--;
-            rem.pop_back();
-        }
-        for(auto it:subs)
-        cout<<it<<" ";
-        cout<<endl;
-    }
-
-    int main()
-    {
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0),cout.tie(0);
 
