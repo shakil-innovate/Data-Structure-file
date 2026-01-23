94f6bfdac8f772664cafaa64a2260dfffd2bc112
shakil-innovate <shakil.innovate@gmail.com>
2026-01-23 09:51:13 +0600

    Refactor knapsack function for improved readability

--- Dynamic_problem/knapsack_1_0.cpp
+++ Dynamic_problem/knapsack_1_0.cpp
@@ -1,36 +1,30 @@
-problem link:https://atcoder.jp/contests/math-and-algorithm/tasks/dp_d?lang=en
+//problem link:https://atcoder.jp/contests/math-and-algorithm/tasks/dp_d?lang=en
 
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define vl vector<ll>
 
-void shakil()
-{
-    ll n,weight;  cin>>n>>weight;
-    vector<ll>w(n+1),p(n+1);
+void shakil(){
+  ll n,weight;  cin>>n>>weight;
+  vl w(n+1),p(n+1);
 
-    for(int i=1;i<=n; i++) cin>>w[i]>>p[i];
+  for(int i=1;i<=n; i++) cin>>w[i]>>p[i];
+  vector<vector<ll>>dp(n+1,vector<ll>(weight+1,0));
 
-    vector<vector<ll>>dp(n+1,vector<ll>(weight+1,0));
-
-    for(int i=0 ; i<=n ;i++)
-    {
-        for(int j=0; j<=weight ; j++)
-        {
-          if(i==0 || j==0)  dp[i][j]=0;
-          else if(w[i]<=j)
-          {
-            dp[i][j]=max(dp[i-1][j],p[i]+dp[i-1][j-w[i]]);
-          }
-          else
-          {
-            dp[i][j]=dp[i-1][j];
-          }
-        }
+  for(int i=0 ; i<=n ;i++){
+    for(int j=0; j<=weight ; j++){
+      if(i==0 || j==0)  dp[i][j]=0;
+      else if(w[i]<=j){
+        dp[i][j]=max(dp[i-1][j],p[i]+dp[i-1][j-w[i]]);
+       }
+      else{
+        dp[i][j]=dp[i-1][j];
+       }
     }
+   }
 
-    cout<<dp[n][weight]<<endl;
-
+  cout<<dp[n][weight]<<endl;
 }
 
 int main()
