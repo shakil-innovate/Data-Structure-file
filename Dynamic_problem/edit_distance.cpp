e536d2ce695fba775c87c0741d9a46fcd33eb09d
shakil-innovate <shakil.innovate@gmail.com>
2026-01-23 09:47:54 +0600

    Refactor edit_distance function for clarity

--- Dynamic_problem/edit_distance.cpp
+++ Dynamic_problem/edit_distance.cpp
@@ -1,28 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define vl vector<ll>
 
-ll edit_distance(string &give,string &convert)
-{
-     vector<vector<ll>>dp(give.size()+1,vector<ll>(convert.size()+1,INT_MAX));
+ll edit_distance(string &give,string &convert){
+  vector<vl>dp(give.size()+1,vl(convert.size()+1,INT_MAX));
 
-   for(int i=0; i<=give.size();i++)
-      for(int j=0 ;j<=convert.size() ; j++)
-      {
-         if(i==0)dp[i][j]=j;
-         else if(j==0)dp[i][j]=i;
+  for(int i=0; i<=give.size();i++)
+    for(int j=0 ;j<=convert.size() ; j++){
+      if(i==0)dp[i][j]=j;
+      else if(j==0)dp[i][j]=i;
 
-         else if(give[i-1]==convert[j-1])
-         {
-            dp[i][j]=dp[i-1][j-1];            //if same char
-         }
-         else
-         {
-            dp[i][j]=min(dp[i][j-1]+1,        //insert
-                          min(dp[i-1][j]+1,   //delete    
-                          dp[i-1][j-1]+1));    //replace
-         }
+      else if(give[i-1]==convert[j-1]){
+        dp[i][j]=dp[i-1][j-1];            //if same char
       }
+      else{
+        dp[i][j]=min(dp[i][j-1]+1,        //insert
+        min(dp[i-1][j]+1,   //delete    
+        dp[i-1][j-1]+1));    //replace
+        }
+    }
 
    return dp[give.size()][convert.size()];
 } 
